29f142b94124f24a4309e4ccedd75800844eee9b
Emerson Saturnino <programador.emerson@hotmail.com>
2022-12-05 09:50:06 -0300

    :recycle: refactor: changing to reduce

--- src/utils/returnFullRuleType.ts
+++ src/utils/returnFullRuleType.ts
@@ -4,11 +4,10 @@ import { TypeCodeEnum } from 'src/enums/type.code';
 import { Rule } from 'src/users/models/rule.model';
 
 const returnFullRuleType = (feature: FeatureCodeEnum): Rule[] => {
-  const fullRule = [];
-  for (const rule of Object.keys(TypeCodeEnum)) {
-    fullRule.push({ action: TypeCodeEnum[rule], subject: feature });
-  }
-  return fullRule;
+  return Object.keys(TypeCodeEnum).reduce((acc, rule) => {
+    acc.push({ action: TypeCodeEnum[rule], subject: feature });
+    return acc;
+  }, []);
 };
 
 export default returnFullRuleType;
